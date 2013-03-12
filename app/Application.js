5a35fd5a81571200daceb4dc79885343b68bec3e
Alfonso Palacio <alfonso.a.palacio@gmail.com>
2013-03-12 13:36:02 -0500

    creating repo

---
+++ app/Application.js
@@ -0,0 +1,7 @@
+Ext.application({
+	name: 'Panda',
+	autoCreateViewPort: true,
+	launch: function(){
+		// This is fired as soon as the page is ready
+	}
+});
---
+++ app/view/SongInfo.js
@@ -0,0 +1,5 @@
+Ext.define('',{
+	extend: 'Ext.panel.Panel',
+	alias:	'widget.songinfo',
+	tpl:	'<h1>About </h1><p></p>'
+});
